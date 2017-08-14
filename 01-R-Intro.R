daa858313e21e20f8c995144e77e4a1f8d78c75b
Rana-ElRobi <eng.rana.abdel-haleem@ieee.org>
2017-08-14 18:57:13 +0200

    added vector example

--- 01-R-Intro.R
+++ 01-R-Intro.R
@@ -3,3 +3,9 @@
 # Install (MASS) package
 library(MASS)
 #-----------------------------
+# CREATE A VECTOR
+vec <- c(1,2,3,4,5,6,7,8)
+# show vector content
+vec #[1] 1 2 3 4 5 6 7 8
+#-----------------------------
+# Create Matrix
